b277ab8c479aaf8ebdb943bdab19018ab6c6383d
HirokiOtani <hf3311@gmail.com>
2021-02-12 11:32:32 +0900

    fix a Procfile

--- server/knexfile.js
+++ server/knexfile.js
@@ -2,7 +2,7 @@ const config = {
   client: "pg",
   connection: {
     connectionString:
-      process.env.DATABASE_URL ||
+      process.env.REACT_APP_DATABASE_URL ||
       `postgres://${process.env.USER}:${process.env.PW}@127.0.0.1:5432/colorname`,
     ssl: process.env.HAS_SSL ? { rejectUnauthorized: false } : undefined,
   },
